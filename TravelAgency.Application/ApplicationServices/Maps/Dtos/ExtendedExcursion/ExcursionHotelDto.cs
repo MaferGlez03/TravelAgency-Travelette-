c32e3cb7c29312fbc28df6eb46b04179af65781f
MaferGlez03 <mariafernandasuarezglez@gmail.com>
2024-04-02 01:36:26 -0400

    excursion delete fixed

---
+++ TravelAgency.Application/ApplicationServices/Maps/Dtos/ExtendedExcursion/ExcursionHotelDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Application.ApplicationServices.Maps.Dtos.ExtendedExcursion
+{
+    public class ExcursionHotelDto
+    {
+        public int HotelId { get; set; }
+        public DateTime ArrivalHotelDay { get; set; }
+    }
+}
